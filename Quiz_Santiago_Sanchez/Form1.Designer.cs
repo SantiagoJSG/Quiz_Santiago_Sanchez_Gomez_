a886dc9e848cd560f4706fcedbb41d9f1c197340
SantiagoJSG <santiagojsanchezg@gmail.com>
2021-09-08 22:26:48 -0500

    Agregar archivos de proyecto.

---
+++ Quiz_Santiago_Sanchez/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace Quiz_Santiago_Sanchez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtdocumento = new System.Windows.Forms.TextBox();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbdocumento = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 30.25F);
+            this.label1.Location = new System.Drawing.Point(247, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Oswald", 20.25F);
+            this.label2.Location = new System.Drawing.Point(63, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo de Documento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Oswald", 20.25F);
+            this.label3.Location = new System.Drawing.Point(171, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Oswald", 20.25F);
+            this.label4.Location = new System.Drawing.Point(192, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 46);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Edad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Oswald", 20.25F);
+            this.label5.Location = new System.Drawing.Point(178, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 46);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Genero:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Oswald", 20.25F);
+            this.label6.Location = new System.Drawing.Point(138, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 46);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Documento:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(302, 286);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Masculino";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(484, 286);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Femenino";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(302, 229);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(253, 31);
+            this.txtname.TabIndex = 8;
+            this.txtname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtdocumento
+            // 
+            this.txtdocumento.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdocumento.Location = new System.Drawing.Point(302, 183);
+            this.txtdocumento.Name = "txtdocumento";
+            this.txtdocumento.Size = new System.Drawing.Size(253, 31);
+            this.txtdocumento.TabIndex = 9;
+            this.txtdocumento.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtedad
+            // 
+            this.txtedad.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtedad.Location = new System.Drawing.Point(302, 321);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(253, 31);
+            this.txtedad.TabIndex = 10;
+            this.txtedad.TextChanged += new System.EventHandler(this.txtedad_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(358, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 36);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbdocumento
+            // 
+            this.cmbdocumento.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbdocumento.FormattingEnabled = true;
+            this.cmbdocumento.Items.AddRange(new object[] {
+            "Tarjeta de Identidad",
+            "Cedula"});
+            this.cmbdocumento.Location = new System.Drawing.Point(302, 136);
+            this.cmbdocumento.Name = "cmbdocumento";
+            this.cmbdocumento.Size = new System.Drawing.Size(253, 35);
+            this.cmbdocumento.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbdocumento);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtdocumento);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtdocumento;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbdocumento;
+    }
+}
+
---
+++ Quiz_Santiago_Sanchez/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Santiago_Sanchez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtdocumento.Text.Length > 0) 
+                {
+                    Convert.ToInt64(txtdocumento.Text);
+                }                    
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Solo números en el apartado de documento y edad");
+                if (txtdocumento.Text.Length > 0)
+                {
+                    txtdocumento.Text = txtdocumento.Text.Remove(txtdocumento.Text.Length - 1, 1);
+                }                
+            }
+        }
+
+        private void txtedad_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtedad.Text.Length > 0)
+                {
+                    Convert.ToInt64(txtedad.Text);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Solo números en el apartado de documento y edad");
+                if (txtedad.Text.Length > 0)
+                {
+                    txtedad.Text = txtedad.Text.Remove(txtedad.Text.Length - 1, 1);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtname.Text.Length == 0)
+                {
+                    return;
+                }
+                if (txtname.Text.Length > 1)
+                {
+                    Convert.ToInt64(txtname.Text.Remove(txtname.Text.Length - 1));
+                }
+                if (txtname.Text.Length == 1) 
+                {
+                    Convert.ToInt64(txtname.Text);
+                }
+                MessageBox.Show("Solo letras en el apartado nombre");
+                if (txtname.Text.Length > 0)
+                {
+                    txtname.Text = txtname.Text.Remove(txtname.Text.Length - 1, 1);
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            ///if (txtname.Text.Length == 1)
+            /// {
+            /// if (txtname.Text.Remove(txtname.Text.Length - 2));
+            ///}
+            /// if (txtname.Text.Remove(txtname.Text.Length - 2)) 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String genero = "";
+            if (radioButton1.Checked) 
+            {
+                genero = "Masculino";
+            }
+            if (radioButton2.Checked)
+            {
+                genero = "Femenino";
+            }
+            MessageBox.Show("Tipo de Documento: " + cmbdocumento.Text + "\nDocumento: " + txtdocumento.Text + "\nNombre: " + txtname.Text + "\nGenero: " + genero + "\nEdad: " + txtedad.Text + "\n");
+        }
+    }
+}
